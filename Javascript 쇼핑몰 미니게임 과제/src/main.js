9a2b40b0290816a87c62ba9f49604b198e619649
Rahyuni <hanrahyun@gmail.com>
2024-07-03 00:54:39 +0900

    JavaScript - handling events

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -22,6 +22,49 @@ function createHTMLString(item) {
     `;
 }
 
+function onButtonClick(event, items) {
+    const dataset = event.target.dataset;
+    const key = dataset.key;
+    const value = dataset.value;
+
+    if(key == null || value == null) {
+        return;
+    }
+
+    const filtered = items.filter(item => item[key] === value);
+    console.log(filtered);
+    displayItems(filtered);
+}
+
+// Handle button click
+/*function onButtonClick(event, items) {
+    const target = event.target;
+    const key = target.dataset.key;
+    const value = target.dataset.value;
+    if (key == null || value == null) {
+        return;
+    }
+    updateItems(items, key, value);
+}
+
+// Make the items matching {key: value} invisible
+function updateItems(items, key, value) {
+    items.forEach(item => {
+        if (item.dataset[key] == value) {
+            item.classList.remove('invisible');
+        } else {
+            item.classList.add('invisible');
+        }
+    });
+}*/
+
+function setEventListeners(items) {
+    const logo = document.querySelector('.logo');
+    const buttons = document.querySelector('.buttons');
+    logo.addEventListener('click', () => displayItems(items));
+    buttons.addEventListener('click', event => onButtonClick(event, items));
+}
+
 // main
 loadItems() // 프로미스로 items를 받아와서
     .then(items => {
