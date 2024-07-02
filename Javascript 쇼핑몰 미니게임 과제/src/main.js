404fb0d0b97aef8cacd47540b1ce25915d13e40d
Rahyuni <hanrahyun@gmail.com>
2024-07-03 00:19:45 +0900

    JavaScript - display items

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -1,8 +1,25 @@
 // Fetch the items from the JSON file
-function loadItems() {
-    return fetch('data/data.json')
-    .then(response => response.json())
-    .then(json => json.items);
+function loadItems() { // loadItems 함수는
+    return fetch('data/data.json') // fetch를 통해 해당 경로의 파일로부터 데이터를 받아
+    .then(response => response.json()) // 성공적으로 받아졌다면 json 형태로 바꾸고
+    .then(json => json.items); // json 안의 items 배열로 바꿔서 반환함
+}
+
+// items 오브젝트를 받아와서 li로 변환 후 하나의 문자열로 만들어서 innerHTML에 추가
+function displayItems(items) {
+    const container = document.querySelector('.items');
+    // html의 items 클래스를 container에
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
+
+// create HTML list item from the given data item
+function createHTMLString(item) {
+    return `
+    <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item__thumbnail">
+        <span class="item__description">${item.gender}, ${item.size}</span>
+    </li>
+    `;
 }
 
 // main
