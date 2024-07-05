5a7e088a3fed1f05f3cf5d1fba0206f9486fc53a
Rahyuni <hanrahyun@gmail.com>
2024-07-05 23:20:47 +0900

    1-3

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,5 +1,13 @@
-# 1단계: 변수 num 선언
+# 1단계
 num = 0
 
-# 2단계: 1에서 3사이의 정수를 입력받는 코드
-input_number = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
+# 2단계와 3단계
+while True:
+    try:
+        input_number = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): "))
+        if input_number not in [1, 2, 3]:
+            print("1, 2, 3 중 하나를 입력하세요")
+        else:
+            break
+    except ValueError:
+        print("정수를 입력하세요")
