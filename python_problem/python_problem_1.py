0eedf6cf5ef11abbee36e13891ec089e4851dc53
Rahyuni <hanrahyun@gmail.com>
2024-07-05 23:46:48 +0900

    1-5

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,16 +1,18 @@
 num = 0
 
-while True:
-    try:
-        input_number = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): "))
-        if input_number not in [1, 2, 3]:
-            print("1, 2, 3 중 하나를 입력하세요")
-        else:
-            break
-    except ValueError:
-        print("정수를 입력하세요")
+def is_valid(player):
+    while True:
+        try:
+            input_number = int(input(f"{player}, 부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): "))
+            if input_number not in [1, 2, 3]:
+                print("1, 2, 3 중 하나를 입력하세요")
+            else:
+                return input_number
+        except ValueError:
+            print("정수를 입력하세요")
 
-# 4단계
-for i in range(input_number):
-    num += 1
-    print(f"playerA : {num}")
+def Game(player, number):
+    global num
+    for i in range(number):
+        num += 1
+        print(f"{player} : {num}")
